8fa400cbafb7acac38df476a0e05c2e80f3c6ab6
prezzerss <23640506@stu.mmu.ac.uk>
2026-05-14 11:02:15 +0100

    Build CRM dashboard

---
+++ src/app/contacts/page.tsx
@@ -0,0 +1,9 @@
+export default function ContactsPage() {
+    return (
+        <div>
+            <h1 className="text-3l font-bold">
+                Contacts
+            </h1>
+        </div>
+    );
+}
---
+++ src/app/emails/page.tsx
@@ -0,0 +1,9 @@
+export default function EmailsPage() {
+    return (
+        <div>
+            <h1 className="text-3l font-bold">
+                Emails
+            </h1>
+        </div>
+    );
+}
